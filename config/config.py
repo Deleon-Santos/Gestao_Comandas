8862eef58dd5bb247cb8d2f262a63673222aefba
Deleon <delps.santos1987@gmail.com>
2025-12-22 22:07:01 -0300

    fix:ajuste para bd remoto, com o codigo local ajustado

--- config/config.py
+++ config/config.py
@@ -6,35 +6,31 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 load_dotenv()
 
-def get_database_session():
-    DATABASE_URL = os.getenv("DATABASE_URL")
-    engine = create_engine(
-        DATABASE_URL,
-        pool_pre_ping=True
-    )
+""" Conexão com o banco de dados remoto, deve esta comentado para rodar localmente"""
+DATABASE_URL = st.secrets.get("DATABASE_URL") 
+if not DATABASE_URL:
 
-    Base = declarative_base()
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-    session = SessionLocal()
-    return session, Base, engine
+    st.error("DATABASE_URL não configurada!")
+    st.stop()
 
-#conexão com o banco de dados remoto
-try:
-    DATABASE_URL = st.secrets.get("DATABASE_URL") 
-    if not DATABASE_URL:
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-        st.error("DATABASE_URL não configurada!")
-        
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"},
+    pool_pre_ping=True
+)
 
-    if DATABASE_URL.startswith("postgres://"):
-        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-    engine = create_engine(
-        DATABASE_URL,
-        connect_args={"sslmode": "require"},
-        pool_pre_ping=True
-    )
-except:
-    
-    session, Base, engine = get_database_session()
 
+""" Conexão com o banco de dados local precisa esta comentado para o uso remoto"""
+# DATABASE_URL = os.getenv("DATABASE_URL")
+# engine = create_engine(
+#     DATABASE_URL,
+#     pool_pre_ping=True
+# )
+
+# Base = declarative_base()
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# session = SessionLocal()
